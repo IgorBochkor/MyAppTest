16c9904c30ed84858b62b7a48f343f7230321dbc
Igor Bochkor <igorbochkor@gmail.com>
2020-07-28 19:10:37 +0300

    added permission.INTERNET, usesClearTraffic on Manifest

--- app/src/main/java/com/demo/myapptest/MainActivity.java
+++ app/src/main/java/com/demo/myapptest/MainActivity.java
@@ -5,12 +5,21 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.demo.myapptest.adapter.EmployeeAdapter;
+import com.demo.myapptest.api.ApiFactory;
+import com.demo.myapptest.api.ApiService;
 import com.demo.myapptest.pojo.Employee;
+import com.demo.myapptest.pojo.EmployeeResponse;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,9 +33,27 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerViewEmployees = findViewById(R.id.RecycleViewEmployee);
         adapter = new EmployeeAdapter();
+        adapter.setEmployees(new ArrayList<Employee>());
         recyclerViewEmployees.setLayoutManager(new LinearLayoutManager(this));
         recyclerViewEmployees.setAdapter(adapter);
 
+        ApiFactory apiFactory = ApiFactory.getInstance();
+        ApiService apiService = apiFactory.getApiService();
+
+        apiService.getEmployees()
+                .subscribeOn(Schedulers.io()) //завантажуй дані в другому програмному потоці
+                .observeOn(AndroidSchedulers.mainThread()) //виводь дані в головному програмному потоці
+                .subscribe(new Consumer<EmployeeResponse>() { //виконується якщо дані завантажилися успішно
+                    @Override
+                    public void accept(EmployeeResponse employeeResponse) throws Exception {
+                        adapter.setEmployees(employeeResponse.getResponse());
+                    }
+                }, new Consumer<Throwable>() { //виконується якщо дані завантажилися неуспішно
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        Toast.makeText(MainActivity.this, "Помилка завантаження даних!", Toast.LENGTH_SHORT).show();
+                    }
+                });
 //        перевірка роботи recycleView
 //        List<Employee> employees = new ArrayList<>();
 //        Employee employee1 = new Employee();
